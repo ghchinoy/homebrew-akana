2985ca583702d48cf052b999607eeda92e7d983a
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-09-27 22:33:28 -0500

    proper shasum256, try #2

--- oauthtest.rb
+++ oauthtest.rb
@@ -2,7 +2,7 @@ class Oauthtest < Formula
   desc "OAuth tester"
   homepage "https://github.com/ghchinoy/oauthtest"
   url "https://github.com/ghchinoy/oauthtest/releases/download/1.0.0/oauthtest-1.0.0.tar.gz"
-  sha256 "8fe6010d1753f5c50632563d4216791dbb182a2596b8ca62bc7404fd7c6eff57"
+  sha256 "95d260adfbf62ea9e0a54c432cb2da58ccc250c0c6dc8c0e30cbf4f7441e9db1"
 
   bottle :unneeded
 
