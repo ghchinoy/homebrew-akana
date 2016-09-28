c6ab800740dce1802a470df07302f441584f7d85
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-09-27 20:25:46 -0500

    thanks and update to Formula

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ This repository is a convenience for installing my cli tools:
 brew update
 brew install ghchinoy/akana/atmotool
 ```
+
+
+## Thanks
+
+.. to [octovore](http://octavore.com/posts/2016/02/15/distributing-go-apps-os-x) for the concise writeup on creating a Homebrew Formula.
--- atmotool.rb
+++ atmotool.rb
@@ -2,7 +2,7 @@ class Atmotool < Formula
   desc "Akana API Platform CLI"
   homepage "https://github.com/ghchinoy/atmotool"
   url "https://github.com/ghchinoy/atmotool/releases/download/1.6.0/atmotool-1.6.0.tar.gz"
-  sha256 "0550f9a20fd4ced3366c190605ec7cd4e0099911e63299d69cfeafb4b4c0bec8"
+  sha256 "dcc2887e10ca3bbc58e877d59feb4c4ae7f6140fbf962b9a722334b464587cfd"
 
   bottle :unneeded
 
