b6b7b7c39ff71d07b0e569c5214f6cb9cb33d5be
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-10-07 15:15:49 -0600

    atmotool 1.7.1

--- atmotool.rb
+++ atmotool.rb
@@ -1,8 +1,8 @@
 class Atmotool < Formula
   desc "Akana API Platform CLI"
   homepage "https://github.com/ghchinoy/atmotool"
-  url "https://github.com/ghchinoy/atmotool/releases/download/1.7.0/atmotool-1.7.0.tar.gz"
-  sha256 "98ee16abdf321c82313e78782463fc67b2efdcf7a4a89ee0e185c31b99fc0be2"
+  url "https://github.com/ghchinoy/atmotool/releases/download/1.7.1/atmotool-1.7.1.tar.gz"
+  sha256 "0ca03c8aeaa4831969a99d84179701da5dfe0a9398711eae532db42e8e3dacc1"
 
   bottle :unneeded
 
