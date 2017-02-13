77d68860ff1ae7d08d057173907def9a7729763a
G. Hussain Chinoy <ghchinoy@gmail.com>
2017-02-12 20:01:14 -0700

    2.2.0

--- oauthtest.rb
+++ oauthtest.rb
@@ -1,8 +1,8 @@
 class Oauthtest < Formula
   desc "OAuth tester"
   homepage "https://github.com/ghchinoy/oauthtest"
-  url "https://github.com/ghchinoy/oauthtest/releases/download/2.0.0/oauthtest-2.0.0.tar.gz"
-  sha256 "7789082256882c890b441e0912528c2eba820c1e34287f74184de9e5cc49a06d"
+  url "https://github.com/ghchinoy/oauthtest/releases/download/2.2.0/oauthtest-2.2.0.tar.gz"
+  sha256 "4cb876c9eb166050715fbefdc6ff4917d6ee779a791fdeb98ba7b5b55aa84bf6"
   bottle :unneeded
 
   def install
